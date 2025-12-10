b657c443d9684a54e29b7966086da33c43b1cec0
Ccy <943239992@qq.com>
2025-12-10 23:42:31 +0800

    为 TaskHub 增加「定时执行任务」能力，支持： 	•	定时执行 单任务 	•	定时执行 DAG 	•	支持 Cron 表达式 	•	API 管理（添加、删除、查询） 	•	运行结果记录与日志

---
+++ server/src/dag/dag_service.cpp
@@ -0,0 +1,38 @@
+#include "dag_service.h"
+#include "runner/taskRunner.h"
+#include "dag/dag_thread_pool.h"
+namespace taskhub::api {
+    DagService::DagService(runner::TaskRunner &runner):
+    _executor(runner)
+    {
+        dag::DagThreadPool::instance().start(4);
+    }
+    DagService &DagService::instance()
+    {
+        static DagService instance(taskhub::runner::TaskRunner::instance());
+        return instance;
+        // TODO: 在此处插入 return 语句
+    }
+    core::TaskResult DagService::runDag(const std::vector<dag::DagTaskSpec> &specs, const dag::DagConfig &config, const dag::DagEventCallbacks &callbacks)
+    {
+        // TODO Step 1：构建 builder，添加所有任务
+        dag::DagBuilder builder;
+        for (const auto& spec : specs) {
+            builder.addTask(spec);
+        }
+
+        // TODO Step 2：validate
+        auto validateResult = builder.validate();
+        if (!validateResult.ok) {
+            core::TaskResult r;
+            r.status = core::TaskStatus::Failed;
+            r.message = validateResult.errorMessage;
+            return r;
+        }
+
+        // TODO Step 3：构建图 + 运行
+        auto graph = builder.build();
+        dag::DagRunContext ctx(config, std::move(graph), callbacks);
+        return _executor.execute(ctx);
+    }
+}
